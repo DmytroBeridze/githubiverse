ae296a814694b93ae5af1bf70dc6e93bb1f7f2a7
DmytroBeridze <98057267+DmytroBeridze@users.noreply.github.com>
2025-03-08 14:08:32 +0200

    added tooltips

--- client/src/components/molecules/AuthStatus/AuthStatus.tsx
+++ client/src/components/molecules/AuthStatus/AuthStatus.tsx
@@ -6,6 +6,8 @@ import { useContext } from "react";
 import { PopupContext } from "../../../context/PopupContext";
 import { FormTypeContext } from "../../../context/FormTypeContext";
 import { NickNameContext } from "../../../context/NickNameContext";
+import { Tooltip } from "react-tooltip";
+import Icon from "../../atoms/Icon/Icon";
 
 export const AuthStatus = () => {
   const popupContext = useContext(PopupContext);
@@ -32,17 +34,30 @@ export const AuthStatus = () => {
   return (
     <div className={styles.authStatus}>
       {nickName ? (
-        <span onClick={signOut}>{nickName}</span>
+        <span
+          onClick={signOut}
+          data-tooltip-id="auth"
+          data-tooltip-content="Logout :("
+        >
+          <Icon name="user" />
+          {nickName}
+        </span>
       ) : (
         <span
           onClick={() => {
             popupHandler();
             setFormType("signup");
           }}
+          data-tooltip-id="auth"
+          data-tooltip-content="Login =)"
         >
           Login
         </span>
       )}
+      <Tooltip
+        id="auth"
+        className={`tooltip ${nickName ? "Logout" : "Login"}`}
+      />
     </div>
   );
 };
